9994de2bbc5a134927a124f4282fa11bc4aa687d
bobby@neoawareness.com <bobby@neoawareness.com@f87b780f-1855-0410-a4d3-fd11cda3f415>
2008-09-27 08:34:14 +0000

    search current directory & LLVM_LIB_SEARCH_PATH for lua_vm_ops.bc file.
    git-svn-id: http://llvm-lua.googlecode.com/svn/trunk@29 f87b780f-1855-0410-a4d3-fd11cda3f415

--- llvm-lua/load_vm_ops.cpp
+++ llvm-lua/load_vm_ops.cpp
@@ -27,12 +27,12 @@
 
 #ifdef USE_BITCODE_FILE
 #include "llvm/Support/MemoryBuffer.h"
+#include "llvm/System/Path.h"
 #include "llvm/Bitcode/ReaderWriter.h"
 #endif
 
 #include <string>
-
-using namespace llvm;
+#include <vector>
 
 #include "load_vm_ops.h"
 
@@ -40,34 +40,60 @@ using namespace llvm;
 #include "lua_vm_ops_module.h"
 #endif
 
-ModuleProvider *load_vm_ops(bool NoLazyCompilation) {
-	ModuleProvider *MP = NULL;
-	Module *theModule = NULL;
+llvm::ModuleProvider *load_vm_ops(bool NoLazyCompilation) {
+	llvm::ModuleProvider *MP = NULL;
+	llvm::Module *module = NULL;
 	std::string error;
 
 #ifdef USE_BITCODE_FILE
-	const char *ops_file="lua_vm_ops.bc";
+	std::string ops_file="lua_vm_ops.bc";
+	std::vector<llvm::sys::Path> paths;
+	llvm::sys::Path tmp(ops_file);
+	bool found = false;
+
+	// check current directory for ops file first.
+	if(!tmp.isBitcodeFile()) {
+		// get bitcode library path.
+		llvm::sys::Path::GetBitcodeLibraryPaths(paths);
+		// search paths for 'lua_vm_ops.bc' file.
+		for(std::vector<llvm::sys::Path>::iterator I=paths.begin(); I != paths.end(); I++) {
+			tmp = *I;
+			tmp.appendComponent(ops_file);
+			if(tmp.isBitcodeFile()) {
+				ops_file = tmp.toString();
+				found = true;
+				break;
+			}
+		}
+	} else {
+		found = true;
+	}
+	if(!found) {
+		printf("Failed to find '%s' file.\n", ops_file.c_str());
+		printf("Please set environment variable 'LLVM_LIB_SEARCH_PATH' to include the path to '%s'\n", ops_file.c_str());
+		exit(1);
+	}
 	// Load in the bitcode file containing the functions for each
 	// bytecode operation.
-	if(llvm::MemoryBuffer* buffer = llvm::MemoryBuffer::getFile(ops_file, &error)) {
+	if(llvm::MemoryBuffer* buffer = llvm::MemoryBuffer::getFile(ops_file.c_str(), &error)) {
 		MP = llvm::getBitcodeModuleProvider(buffer, &error);
 		if(!MP) delete buffer;
 	}
 	if(!MP) {
-		printf("Failed to parse %s file: %s\n", ops_file, error.c_str());
+		printf("Failed to parse %s file: %s\n", ops_file.c_str(), error.c_str());
 		exit(1);
 	}
 	// Get Module from ModuleProvider.
 	if(NoLazyCompilation) {
-		theModule = MP->materializeModule(&error);
-		if(!theModule) {
-			printf("Failed to read %s file: %s\n", ops_file, error.c_str());
+		module = MP->materializeModule(&error);
+		if(!module) {
+			printf("Failed to read %s file: %s\n", ops_file.c_str(), error.c_str());
 			exit(1);
 		}
 	}
 #else
-	theModule = make_lua_vm_ops();
-	MP = new llvm::ExistingModuleProvider(theModule);
+	module = make_lua_vm_ops();
+	MP = new llvm::ExistingModuleProvider(module);
 #endif
 
 	return MP;
