1592c1df9284b2d8e0bf8578841db4b78510026b
bobby@neoawareness.com <bobby@neoawareness.com@f87b780f-1855-0410-a4d3-fd11cda3f415>
2008-10-08 06:03:18 +0000

    remove '-foptimize-sibling-calls' disabled '-march=athlon64'
    git-svn-id: http://llvm-lua.googlecode.com/svn/trunk@48 f87b780f-1855-0410-a4d3-fd11cda3f415

--- llvm-lua/compile.sh
+++ llvm-lua/compile.sh
@@ -6,13 +6,14 @@ if [[ $DEBUG == "1" ]]; then
 	#CFLAGS=" -O0 -ggdb "
 	LUA_FLAGS=" -O3 "
 	#LUA_FLAGS=" -O3 -do-not-inline-opcodes -dump-functions "
-	CFLAGS=" -ggdb -O3 -foptimize-sibling-calls -fomit-frame-pointer -pipe -Wall "
+	CFLAGS=" -ggdb -O3 -fomit-frame-pointer -pipe -Wall "
 	OPT_FLAGS=" -disable-opt "
 	LLC_FLAGS=" "
 else
 	LUA_FLAGS=" -O3 "
 	#CFLAGS=" -ggdb -march=athlon64 -O3 -fomit-frame-pointer -pipe -Wall "
-	CFLAGS=" -march=athlon64 -O3 -foptimize-sibling-calls -fomit-frame-pointer -pipe -Wall "
+	#CFLAGS=" -march=athlon64 -O3 -fomit-frame-pointer -pipe -Wall "
+	CFLAGS=" -O3 -fomit-frame-pointer -pipe -Wall "
 	OPT_FLAGS=" -O3 -std-compile-opts -tailcallelim -tailduplicate "
 	LLC_FLAGS=" -tailcallopt "
 fi
