5a6efa99b9d337dc04e0efb71ff43612b84d87f1
bobby@neoawareness.com <bobby@neoawareness.com@f87b780f-1855-0410-a4d3-fd11cda3f415>
2009-11-19 00:25:15 +0000

    possible fix for linking error on mac osx.
    git-svn-id: http://llvm-lua.googlecode.com/svn/trunk@115 f87b780f-1855-0410-a4d3-fd11cda3f415

--- cmake/FindLLVM.cmake
+++ cmake/FindLLVM.cmake
@@ -19,16 +19,15 @@ execute_process(
 	OUTPUT_STRIP_TRAILING_WHITESPACE
 )
 execute_process(
-	COMMAND ${LLVM_CONFIG_EXECUTABLE} --libfiles core jit native linker bitreader bitwriter ipo
+	COMMAND ${LLVM_CONFIG_EXECUTABLE} --libs core jit native linker bitreader bitwriter ipo
 	OUTPUT_VARIABLE LLVM_JIT_LIBS
 	OUTPUT_STRIP_TRAILING_WHITESPACE
 )
 execute_process(
-	COMMAND ${LLVM_CONFIG_EXECUTABLE} --libfiles all
+	COMMAND ${LLVM_CONFIG_EXECUTABLE} --libs all
 	OUTPUT_VARIABLE LLVM_ALL_LIBS
 	OUTPUT_STRIP_TRAILING_WHITESPACE
 )
-
-string(REPLACE " " ";" LLVM_ALL_LIBS ${LLVM_ALL_LIBS})
-string(REPLACE " " ";" LLVM_JIT_LIBS ${LLVM_JIT_LIBS})
+message("llvm jit libs: " ${LLVM_JIT_LIBS})
+message("llvm all libs: " ${LLVM_ALL_LIBS})
 
