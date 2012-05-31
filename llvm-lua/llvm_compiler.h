6323137d1fa3876cee5a283acb1ff6de9e39a687
Dennis Schridde <devurandom@gmx.net>
2012-05-31 15:28:53 +0200

    Make LLVM_LUA_VERSION and LLVM_LUA_COPYRIGHT configurable via cmake
    Signed-off-by: Robert G. Jakabosky <bobby@sharedrealm.com>

--- llvm-lua/llvm_compiler.h
+++ llvm-lua/llvm_compiler.h
@@ -1,16 +1,16 @@
 /*
   Copyright (c) 2009 Robert G. Jakabosky
-  
+
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
   in the Software without restriction, including without limitation the rights
   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
   copies of the Software, and to permit persons to whom the Software is
   furnished to do so, subject to the following conditions:
-  
+
   The above copyright notice and this permission notice shall be included in
   all copies or substantial portions of the Software.
-  
+
   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,9 +26,7 @@
 #define llvm_compiler_h
 
 #include "lua_core.h"
-
-#define LLVM_LUA_VERSION "llvm-lua 1.2.0"
-#define LLVM_LUA_COPYRIGHT "Copyright (C) 2008-2009 Robert G. Jakabosky, http://code.google.com/p/llvm-lua/"
+#include "llvm_lua_config.h"
 
 #ifdef __cplusplus
 extern "C" {
