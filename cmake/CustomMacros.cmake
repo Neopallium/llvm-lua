618beebb8b4e707f0cdd84b8382be276db5245aa
Dennis Schridde <devurandom@gmx.net>
2012-06-02 12:55:42 +0200

    Amend b4ff1b791d9e: Inform clang about additional include directories and so it finds the generated llvm_lua_config.h
    Signed-off-by: Robert G. Jakabosky <bobby@sharedrealm.com>

--- cmake/CustomMacros.cmake
+++ cmake/CustomMacros.cmake
@@ -1,12 +1,18 @@
 
 macro(add_llvm_bc_library _target)
+	set(_llvm_cflags)
+	get_property(_idirs DIRECTORY PROPERTY INCLUDE_DIRECTORIES)
+	foreach(_idir ${_idirs})
+		set(_llvm_cflags ${_llvm_cflags} -I${_idir})
+	endforeach(_idir)
+
 	if(${ARGC} GREATER 2)
 		set(_bc_files)
 		foreach(_file ${ARGN})
 			set(_bc_file "${CMAKE_CURRENT_BINARY_DIR}/${_file}.bc")
 			set(_bc_files ${_bc_files} ${_bc_file})
 			add_custom_command(OUTPUT ${_bc_file}
-				COMMAND ${LLVM_CC} ARGS ${BC_CFLAGS} -o ${_bc_file} ${_file}
+				COMMAND ${LLVM_CC} ARGS ${BC_CFLAGS} ${_llvm_cflags} -o ${_bc_file} ${_file}
 				WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
 				DEPENDS ${_file}
 			)
@@ -18,7 +24,7 @@ macro(add_llvm_bc_library _target)
 		)
 	else(${ARGC} GREATER 2)
 		add_custom_command(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${_target}.bc
-			COMMAND ${LLVM_CC} ARGS ${BC_CFLAGS} -o ${CMAKE_CURRENT_BINARY_DIR}/${_target}.bc ${ARGV1}
+			COMMAND ${LLVM_CC} ARGS ${BC_CFLAGS} ${_llvm_cflags} -o ${CMAKE_CURRENT_BINARY_DIR}/${_target}.bc ${ARGV1}
 			WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
 			DEPENDS ${ARGV1}
 		)
